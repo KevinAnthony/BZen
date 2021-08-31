b089c0b492b58bff074cb449d189d6fa181324c6
Kevin Anthony <kevin.s.anthony@gmail.com>
2021-08-30 21:59:09 -0400

    added DB types

--- bzenlib/model/types.go
+++ bzenlib/model/types.go
@@ -2,23 +2,57 @@ package model
 
 import "time"
 
-type BoardgameType string
+type (
+	BoardgameType string
+	ContainerType string
+)
 
 const (
-	BGTBase BoardgameType = "base"
+	BGTBase      BoardgameType = "base"
 	BGTExpansion BoardgameType = "expansion"
-	BGTAddon BoardgameType = "addon"
+	BGTAddon     BoardgameType = "addon"
+)
+
+const (
+	CTShelf      ContainerType = "shelf"
+	CTSnapTopBox ContainerType = "snap_top_box"
+	CTBag        ContainerType = "bag"
 )
 
 type Boardgame struct {
 	// nolint: structcheck, unused
 	tableName struct{} `pg:"bzen.boardgame,alias:boardgame"`
 
-	ID string
-	BGGID string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time
-	Name string
-	Type BoardgameType
+	ID        string        `pg:"id,pk,type:uuid"`
+	CreatedAt time.Time     `pg:"created_at"`
+	UpdatedAt time.Time     `pg:"updated_at"`
+	DeletedAt *time.Time    `pg:"deleted_at,soft_delete"`
+	Name      string        `pg:"name"`
+	Type      BoardgameType `pg:"type,type:bzen.boardgame_type_t"`
+	UPC       *string       `pg:"upc"`
+	BggID     string        `pg:"bgg_id"`
+}
+
+type Container struct {
+	// nolint: structcheck, unused
+	tableName struct{} `pg:"bzen.container,alias:container"`
+
+	ID        string        `pg:"id,pk,type:uuid"`
+	CreatedAt time.Time     `pg:"created_at"`
+	UpdatedAt time.Time     `pg:"updated_at"`
+	DeletedAt *time.Time    `pg:"deleted_at,soft_delete"`
+	Name      string        `pg:"name"`
+	Type      ContainerType `pg:"type,type:bzen.container_type_t"`
+	Location  Area          `pg:"rel:has-one,join_fk:area_id"`
+}
+
+type Area struct {
+	// nolint: structcheck, unused
+	tableName struct{} `pg:"bzen.area,alias:area,"`
+
+	ID        string     `pg:"id,pk,type:uuid"`
+	CreatedAt time.Time  `pg:"created_at"`
+	UpdatedAt time.Time  `pg:"updated_at"`
+	DeletedAt *time.Time `pg:"deleted_at,soft_delete"`
+	Name      string     `pg:"name"`
 }
